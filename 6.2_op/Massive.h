9345caa8e91b0e8a596c9231b03779e0c382bd6a
Mitzengendler <54206076+Mitzengendler@users.noreply.github.com>
2020-06-09 18:19:36 +0300

    template + rand mas

---
+++ 6.2_op/Massive.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <ctime>
+
+using namespace std;
+
+template<class T>
+class Massive
+{
+private:
+	vector<T> mas;
+	int numOfEll;
+public:
+	Massive(int n);
+	~Massive();
+	void showMas();
+};
+
+template<class T>
+inline Massive<T>::Massive(int n)
+{
+	this->numOfEll = n;
+	srand(time(NULL));
+	for (int i = 0; i < this->numOfEll; i++) {
+		mas.push_back(rand() % 10 + 1);
+		//mas.push_back(21.5);
+	}
+
+	//
+
+
+}
+
+template<class T>
+inline Massive<T>::~Massive()
+{
+
+}
+
+template<class T>
+inline void Massive<T>::showMas()
+{
+	for (int i = 0; i < this->mas.size(); i++) {
+		cout << this->mas[i] << "  ";
+	}
+	cout << endl;
+}
+
